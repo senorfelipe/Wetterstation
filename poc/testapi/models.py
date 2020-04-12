e07998985496d1741f53d150b1d6b6a18309bc9b
Philipp Barth <philipp-barth13@web.de>
2020-04-12 10:09:57 +0200

    added new model

--- poc/testapi/models.py
+++ poc/testapi/models.py
@@ -4,8 +4,16 @@ from django.db import models
 # Create your models here.
 
 class Temperature(models.Model):
+    time = models.DateTimeField(auto_now=True)
     value = models.DecimalField(max_digits=5, decimal_places=2)
-    unit = models.CharField(max_length=20)
 
     def __str__(self):
-        return str(self.value) + ' ' + self.unit
+        return str(self.time) + ' ' + str(self.value)
+
+
+class ImageReferences(models.Model):
+    time = models.DateTimeField(auto_now_add=True)
+    file_path = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.file_path
