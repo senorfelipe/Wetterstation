260a8db10287adf530accfc018a7352001fdabd5
Justin Schirdewahn <s78878@informatik.htw-dresden.de>
2020-05-04 18:25:18 +0200

    httpclient

--- src/http-client/http-client.py
+++ src/http-client/http-client.py
@@ -2,11 +2,25 @@ import os
 import requests
 
 os.environ['NO PROXY'] = '127.0.0.1'
-r = requests.get('http://127.0.0.1:8000/api/wind/')
-print(r.status_code)
-print(r.headers['Content-Type'])
-print(r.text)
 
-p = requests.post('http://127.0.0.1:8000/api/wind')
-print(help(p.content))
+class HttpClient:
+    def __init__(self, url):
+        self.url = url
+
+    def post(self, value):
+        requests.post(self.url, data=value)
+
+    def get(self):
+        r = requests.get(self.url)
+        print(r.text)
+
+temperature = {'value': '144.6'}
+wind = {'speed': '100.0', 'direction': '666.0'}
+temp = HttpClient('http://127.0.0.1:8000/api/temps/')
+temp.post(temperature)
+temp.get()
+windig = HttpClient('http://127.0.0.1:8000/api/wind/')
+windig.post(wind)
+windig.get()
+
 
