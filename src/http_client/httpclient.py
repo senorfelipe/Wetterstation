e8e8180c44cf205cb7ca75ef6e8db5e7664a8cb8
Philipp Barth <philipp-barth13@web.de>
2020-05-18 12:08:40 +0200

    implemented image post for http client

--- src/http_client/httpclient.py
+++ src/http_client/httpclient.py
@@ -9,21 +9,31 @@ class HttpClient:
     def __init__(self, url):
         self.url = url
 
-    def post(self, data):
+    def post_data(self, data):
         r = requests.post(self.url, data=data)
         return r.status_code
+
+    def post_file(self, file_path, data):
+        with open(file_path, 'rb') as payload:
+            image = {'image': payload}
+            r = requests.post(self.url, files=image, data=data)
+            return r.status_code
 
     def get(self, query_params=''):
         r = requests.get(self.url, params=query_params)
         return [r.text, r.status_code]
 
 
-timestamp = '2020-05-18T11:00:00Z'
-temperature = {'value': '144.6', 'time': timestamp}
-wind = {'speed': '100.0', 'direction': '666.0', 'time': timestamp}
-temp = HttpClient('http://127.0.0.1:8000/api/temps/')
-temp.post(temperature)
-temp.get()
-windig = HttpClient('http://127.0.0.1:8000/api/wind/')
-windig.post(wind)
-print(windig.get({'month': 5}))
+#
+# temperature = {'degrees': '144.6', 'time': '2020-04-20T21:55:38.20Z'}
+# wind = {'speed': '100.0', 'direction': '666.0', 'time': '2020-04-20T21:55:38.20Z'}
+# temp = HttpClient('http://localhost:8000/api/temps/')
+# temp.post_data(temperature)
+# temp.get()
+# windig = HttpClient('http://localhost:8000/api/wind/')
+# windig.post_data(wind)
+# print(windig.get({'start': "2020-04-30"}))
+
+image = HttpClient('http://localhost:8000/api/images/')
+status = image.post_file(file_path='example.jpg', data={'time': '2020-04-20T21:55:38.20Z'})
+print(status)
