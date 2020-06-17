07022bdecc28c31f665b81daebcedccc49a78983
Alexander Schoch <s78797@informatik.htw-dresden.de>
2020-06-17 16:26:33 +0200

    removed backend

--- src/wetterstation/weather/restapi/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Temperature, Wind, Image
-
-admin.site.register(Temperature)
-admin.site.register(Wind)
-admin.site.register(Image)
