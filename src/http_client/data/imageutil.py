09980d21bc1ad3c6e9a34b57a088a67355fb021d
Clemens Kujus <clemens.kujus@htw-dresden.de>
2020-07-27 00:07:50 +0200

    Add new images

--- src/http_client/data/imageutil.py
+++ src/http_client/data/imageutil.py
@@ -5,11 +5,11 @@ from time import time
 import requests
 from PIL import Image
 
-url = 'https://source.unsplash.com/1600x900/?lawn,landscape,meadow'
+url = 'https://source.unsplash.com/1600x900/?grass'
 for i in range(1, 15, 1):
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
-    filename = str((int(time())-24*3600) + random.randrange(1, 200)) + '.jpeg'
+    filename = str(int(time()) + random.randrange(1, 200)) + '.jpeg'
     img = img.convert('RGB')
     img.save('mock/' + filename)
     print('saved ' + str(i) + '.' + 'image: ' + filename)
