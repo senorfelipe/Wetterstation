774fd87fccad643ce9fd53fba86353b2ab00a19c
Alexander Schoch <s78797@informatik.htw-dresden.de>
2020-08-10 16:24:26 +0200

    minor change

--- src/frontend/SE-draft/src/app/admin.service.ts
+++ src/frontend/SE-draft/src/app/admin.service.ts
@@ -1,30 +1,33 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import { catchError } from 'rxjs/operators'; 
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class AdminService {
+
     /** @private URL der REST-Schnittstellee */
     private apiUrl = 'http://localhost:8000/api/';
+
     /**@private Http-Client */
     private http: HttpClient;
+    
     /**@private Error */
     private error;
 
     constructor(http: HttpClient) {
-      this.http = http;
+        this.http = http;
     }
-/**
- * 
- * @param user Benutzername
- * @param pwd Passwort
- * @returns post-request des Authentifizerungtoken
- */
-    provideToken(user, pwd){
-        return this.http.post<AuthToken[]>(this.apiUrl + 'token/', {"username":user, "password":pwd});;
+    /**
+     * 
+     * @param user Benutzername
+     * @param pwd Passwort
+     * @returns post-request des Authentifizerungtoken
+     */
+    provideToken(user, pwd) {
+        return this.http.post<AuthToken[]>(this.apiUrl + 'token/', { "username": user, "password": pwd });;
     }
 }
 
@@ -34,4 +37,4 @@ export interface AuthToken {
     refresh: String;
     /**Zugriffsfreigabe */
     access: String;
-  }
+}
