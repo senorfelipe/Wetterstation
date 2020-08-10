650fbe23d58aada28208eb4ee8f155bb5175df64
Justin Schirdewahn <s78878@informatik.htw-dresden.de>
2020-08-11 01:38:36 +0200

    zusa

--- src/frontend/SE-draft/src/app/admin.service.ts
+++ src/frontend/SE-draft/src/app/admin.service.ts
@@ -1,40 +1,56 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { catchError } from 'rxjs/operators';
+import { catchError } from "rxjs/operators";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: "root",
 })
-
 export class AdminService {
+  /**
+   * @private URL der REST-Schnittstellee
+   */
+  private apiUrl = "http://localhost:8000/api/";
 
-    /** @private URL der REST-Schnittstellee */
-    private apiUrl = 'http://localhost:8000/api/';
+  /**
+   * @private Http-Client
+   */
+  private http: HttpClient;
 
-    /**@private Http-Client */
-    private http: HttpClient;
-    
-    /**@private Error */
-    private error;
+  /**
+   * @private Error
+   */
+  private error;
 
-    constructor(http: HttpClient) {
-        this.http = http;
-    }
-    /**
-     * 
-     * @param user Benutzername
-     * @param pwd Passwort
-     * @returns post-request des Authentifizerungtoken
-     */
-    provideToken(user, pwd) {
-        return this.http.post<AuthToken[]>(this.apiUrl + 'token/', { "username": user, "password": pwd });;
-    }
+  /**
+   *
+   * @param http HttpClient
+   */
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+  /**
+   * @param user Benutzername
+   * @param pwd Passwort
+   * @returns post-request des Authentifizerungtoken
+   */
+  provideToken(user, pwd) {
+    return this.http.post<AuthToken[]>(this.apiUrl + "token/", {
+      username: user,
+      password: pwd,
+    });
+  }
 }
 
-/**Interface für den Authentifizierungstoken */
+/**
+ * Interface für den Authentifizierungstoken
+ */
 export interface AuthToken {
-    /**Refresh */
-    refresh: String;
-    /**Zugriffsfreigabe */
-    access: String;
+  /**
+   * Refresh
+   */
+  refresh: String;
+  /**
+   * Zugriffsfreigabe
+   */
+  access: String;
 }
